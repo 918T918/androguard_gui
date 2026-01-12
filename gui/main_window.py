ff7a545733312dbdb0832bfdeaf4828cb5a1c972
918T918 <Damnitanner@gmail.com>
2026-01-12 14:05:04 -0600

    another fucking missing import, QFont this time, kill me now

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QTabWidget, 
                              QFileDialog, QToolBar, QStatusBar, QMessageBox, QDockWidget, QMenu, QApplication, QProgressBar, QTextEdit)
-from PyQt6.QtGui import QAction, QIcon, QKeySequence
+from PyQt6.QtGui import QAction, QIcon, QKeySequence, QFont
 from PyQt6.QtCore import Qt, QSize, QSettings
 import os
 
